3935adfa7d81c483ec08eb0a6b7366b46e156857
hatimorad <hatim.o.rad@gmail.com>
2014-10-31 21:15:02 +0100

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/MainScene.m
+++ FlappyBird.spritebuilder/Source/MainScene.m
@@ -120,26 +120,28 @@
 #pragma mark - Update
 
 // move and loop the bushes
-for (CCNode *bush in _bushes){
+for (CCNode *bush in _bushes) {
     // move the bush
     bush.position = ccp(bush.position.x -
-                        (Character.physicsBody.velocity * delta), bush.position.y);
-                        
-        //if the left corner is one complete width off the screen,
-        //move it to the right
-                        if (bush.position.x <= (1 * bush.contentSize.width)){
-                            bush.position = ccp(bush.position.x +
-                                                2 * bush.contentSize.width, bush.position.y);
-                        }
+                        (character.physicsBody.velocity.x * delta), bush.position.y);
+    
+    // if the left corner is one complete width off the screen,
+    // move it to the right
+    if (bush.position.x <= (-1 * bush.contentSize.width)) {
+        bush.position = ccp(bush.position.x +
+                            2 * bush.contentSize.width, bush.position.y);
+    }
 }
+
 // move and loop the clouds
-for (CCNode *cloud in _clouds){
-    //move the cloud
+for (CCNode *cloud in _clouds) {
+    // move the cloud
     cloud.position = ccp(cloud.position.x -
-                         (Character.physicsBody.velocity * delta), cloud.position.y);
-        //if the left corner is one complete with off the screen,
-        // move it to the right
-    if  (cloud.position.x <= (1 * cloud.contentSize.width)){
+                         (character.physicsBody.velocity.x * delta), cloud.position.y);
+    
+    // if the left corner is one complete width off the screen,
+    // move it to the right
+    if (cloud.position.x <= (-1 * cloud.contentSize.width)) {
         cloud.position = ccp(cloud.position.x +
                              2 * cloud.contentSize.width, cloud.position.y);
     }
